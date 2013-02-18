694cefd1434fb2afee5aa06af8a9fd73c5036378
Vashishtha Jogi <vashishthajogi@gmail.com>
2013-02-17 19:02:24 -0800

    email template changes

--- server/emailutil.py
+++ server/emailutil.py
@@ -3,11 +3,11 @@ from django.template.loader import get_template
 from django.template import Context
 
 
-FROM_EMAIL = "admin@listypedia.com"
+FROM_EMAIL = "support@listypedia.com"
 
 
 def send_welcome_email(user):
-    email_template = get_template('email/welcome_email.html')
+    email_template = get_template('email/welcome.html')
     context = Context({'user': user})
     email_msg = email_template.render(context)
     subject = "Welcome to Listypedia!"
@@ -20,7 +20,7 @@ def send_welcome_email(user):
 
 
 def send_follow_confirmation_email(user, list):
-    email_template = get_template('email/follow_confirmation_email.html')
+    email_template = get_template('email/follow_confirmation.html')
     context = Context({'user': user, 'list': list})
     email_msg = email_template.render(context)
     subject = "You are now following list - %s" % list.name
@@ -31,9 +31,10 @@ def send_follow_confirmation_email(user, list):
     msg.content_subtype = "html"
     msg.send(fail_silently=True)
 
+
 def send_collabaration_invitation_email(user, list, collabaration_invitation):
-    email_template = get_template('email/collaboration_invitation_email.html')
-    context = Context({'user': user, 'list': list,'collabaration_invitation':collabaration_invitation})
+    email_template = get_template('email/collaboration_invitation.html')
+    context = Context({'user': user, 'list': list, 'collabaration_invitation': collabaration_invitation})
     email_msg = email_template.render(context)
     subject = "You are invited to collaborate on - %s" % list.name
     from_email = FROM_EMAIL
@@ -45,7 +46,7 @@ def send_collabaration_invitation_email(user, list, collabaration_invitation):
 
 
 def send_item_add_notification_email(user, list, item, followers):
-    email_template = get_template('email/item_add_notification_email.html')
+    email_template = get_template('email/item_add_notification.html')
     context = Context({'user': user, 'list': list, 'item': item})
     email_msg = email_template.render(context)
     subject = "listypedia notification"
