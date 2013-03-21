39afca95e45ce4bce97b49ca146a5491a36050d1
Vashishtha Jogi <vashishthajogi@gmail.com>
2013-03-20 18:35:56 -0700

    -Fix user lists again (previous one did not work) -Upgrade django to 1.5

--- server/managers.py
+++ server/managers.py
@@ -8,13 +8,15 @@ class ListManager(models.Manager):
         return super(ListManager, self).create(name=name, description=description, user=user)
 
     def get_lists_by_user(self, user, collaborator=True):
-        query = (Q(user=user) & Q(active=True))
+        query = Q(active=True)
         if collaborator:
-            query = query | Q(collaborator=user)
+            query = query & (Q(collaborator=user) | Q(user=user))
+        else:
+            query = query & Q(user=user)
         return super(ListManager, self).filter(query)
 
     def get_followed_lists_by_user(self, user):
-        return super(ListManager, self).filter(follower=user)
+        return super(ListManager, self).filter(follower=user, active=True)
 
     def get_collaborated_lists_by_user(self, user):
         return super(ListManager, self).filter(collaborator=user)
